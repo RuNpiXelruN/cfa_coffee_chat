90e4dba12db1a7815cc7998d5e3bf9a7f012b060
Justin Davidson <jutstindavidson23@gmail.com>
2016-06-21 20:33:45 +1000

    Styled follow and tutor buttons on profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,17 +2,17 @@ module UsersHelper
 
   def follow_button user
     if current_user.following? user
-      button_to "Unfollow", user_follow_path(user), method: :delete
+      button_to("Unfollow", user_follow_path(user), method: :delete, class: "btn btn-danger")
     else
-      button_to "Follow", user_follow_path(user)
+      button_to("Follow",user_follow_path(user), class: "btn btn-primary")
     end
   end
 
   def tutor_button user
     if current_user.is_connected? user
-      button_to "Cancel Tutoring", user_tutor_path(user), method: :delete
+      button_to("Cancel Tutoring", user_tutor_path(user), method: :delete, class: "btn btn-warning")
     else
-      button_to "Add as Tutee!", user_tutor_path(user)
+      button_to("Add as Tutee!", user_tutor_path(user), class: "btn btn-success")
     end
   end
 end
