82139187b1cf24a82317d44f78623dee88b513fe
Justin Davidson <jutstindavidson23@gmail.com>
2016-06-27 17:37:40 +1000

    fixed follower in migration

--- db/migrate/20160615113231_create_following_relationships.rb
+++ db/migrate/20160615113231_create_following_relationships.rb
@@ -1,8 +1,8 @@
 class CreateFollowingRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :following_relationships do |t|
-      t.references :follower, foreign_key: true
-      t.references :followed_user, foreign_key: true
+      t.integer :follower_id
+      t.integer :followed_user_id
 
       t.timestamps
     end
